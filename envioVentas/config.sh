7e1a2ba4171803c6b7c7440d2eaf04fe21382934
Daniel <daniel.humberto@tudespensa.com>
2018-01-22 19:22:33 +0100

    Ya se validan las banderas y se copian los archivos. Solo falta subirlos al servidor

--- envioVentas/config.sh
+++ envioVentas/config.sh
@@ -1,26 +1,99 @@
 #!/bin/bash
 
 
-mahouUsu="tudespensa"
-mahouPass="Des4080pr1"
+MAHOU_HOST="ftp.msmfiles.net"
+MAHOU_USU="tudespensa"
+MAHOU_PASS="Des4080pr1"
 
-nielsenUsu="tudespensa"
-nielsenPass="3qq7#vPr"
+NIELSEN_HOST="138.108.98.23"
+NIELSEN_USU="tudespensa"
+NIELSEN_PASS="3qq7#vPr"
 
-ficheroMahou="mahou"
-ficheroNielsenTotal="nielsen_total"
-ficheroNielsenProvincias="nielsen_provincia"
+FRONT4_USU="root"
+FRONT4_PASS="&4mwh6D28&Aw192"
 
-rutaArchivosOriginales="/mnt/tudespensa0/vhosts/tudespensa.com/httpdocsv4/var/rep/temp/"
+FICHERO_MAHOU="mahou.conf"
+FICHERO_NIELSEN_TOTAL="nielsen_total"
+FICHERO_NIELSEN_PROVINCIAS="nielsen_provincia"
 
-rutaTmp="/mnt/tudespensa0/vhosts/satelitesTudespensa/envioVentas/archivos/tmp/"
+RUTA_ORIGINALES="/mnt/tudespensa0/vhosts/tudespensa.com/httpdocsv4/var/rep/temp/"
 
-ext=".txt"
-
-ficheroMahou="_VENTAS_MAHOU"
-
-ficheroNielsenTotal="ventas_semana_"
-
-ficheroNielsenProvincias="ventas_semana_provincias_"
+RUTA_TMP="/mnt/tudespensa0/vhosts/satelitesTudespensa/envioVentas/archivos/tmp/"
+RUTA_FAIL="/mnt/tudespensa0/vhosts/satelitesTudespensa/envioVentas/archivos/fallidos/"
+RUTA_OK="/mnt/tudespensa0/vhosts/satelitesTudespensa/envioVentas/archivos/correctos/"
 
 
+function procesoEnviarArchivo() {
+
+    host="$1"
+    user="$2"
+    passwd="$3"
+    file="$4"
+
+archivoOrg=$RUTA_ORIGINALES$file
+archivoTmp=$RUTA_TMP$file
+
+
+if [ -f $archivoOrg ]
+then
+
+    cp $archivoOrg $archivoTmp
+
+    if [ -f $archivoTmp ]
+    then
+
+        enviarArchivoFtp $host $user $passwd $archivoTmp $file
+
+    else
+
+        echo "ERROR ARCHIVO NO EXISTE 2: "$archivoTmp
+
+    fi
+
+else
+
+    echo "ERROR ARCHIVO NO EXISTE 1: "$archivoOrg
+
+fi
+
+}
+
+function enviarArchivoFtp() {
+
+    host="$1"
+    user="$2"
+    passwd="$3"
+    file_put="$4"
+    file_get="$5"
+
+    echo $host
+    echo $user
+    echo $passwd
+    echo $file_put
+    echo $file_get
+
+sftp $user@$host:$passwd<<GET_FILES
+put $file_put
+quit
+GET_FILES
+
+
+}
+
+function enviarArchivoFtpOLD() {
+
+    host="$1"
+    user="$2"
+    passwd="$3"
+    file_put="$4"
+    file_get="$5"
+
+ftp -n $host<<FTP_CONN
+    quote USER $user
+    quote PASS $passwd
+    binary
+    put $file_put
+#    get $file_get
+    quit
+FTP_CONN
+}
--- envioVentas/envio.sh
+++ envioVentas/envio.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
 source ./config.sh
-source ./ftpTool.sh
 
+ventasMauho=`cat $RUTA_ORIGINALES$FICHERO_MAHOU`
+ventasNielsenTotal=`cat $RUTA_ORIGINALES$FICHERO_NIELSEN_TOTAL`
+ventasNielsenProvincia=`cat $RUTA_ORIGINALES$FICHERO_NIELSEN_PROVINCIAS`
 
-enviarArchivoFtp '1' '2' '3' '4'
+#cp $RUTA_ORIGINALES$ventasMauho $RUTA_TMP$ventasMauho
+#cp $RUTA_ORIGINALES$ventasNielsenTotal $RUTA_TMP$ventasNielsenTotal
+#cp $RUTA_ORIGINALES$ventasNielsenProvincia $RUTA_TMP$ventasNielsenProvincia
+
+host=$MAHOU_HOST
+user=$MAHOU_USU
+passwd=$MAHOU_PASS
+archivo=$ventasMauho
+
+#echo $host
+#echo $user
+#echo $passwd
+#echo $archivo
+
+procesoEnviarArchivo $host $user $passwd $archivo
 
 echo "FIN"
+
